5d1b10fc17b0fb66e7179b811feed0132d27b42a
Foysal Khandakar Joy <khjoyfoysal@gmail.com>
2021-06-01 19:41:14 +0600

    modified views py html

--- todo/views.py
+++ todo/views.py
@@ -68,6 +68,15 @@ def currenttodos(request):
     return render(request, 'todo/currenttodos.html', {'todos':todos})
 
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
-    form = TodoForm(instance=todo)
-    return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad Info'})
